c3dc2d684e1f2849be845f9c6e20a236aecafb15
Dariusz Danielewski <dydek@hotmail.com>
2020-05-23 22:14:02 -0400

    Improved build file

--- build-nuget-packages.ps1
+++ build-nuget-packages.ps1
@@ -1,8 +1,8 @@
-Param(
-[Parameter(Mandatory=$True)]
-[string]$versionSuffix
-)
-cd .\BDev.Net.Dns
+
+[xml]$projectFile = Get-Content Bdev.Net.Dns\Bdev.Net.Dns.csproj
+$versionSuffix = $projectFile.Project.PropertyGroup.Version
+
+pushd .\BDev.Net.Dns
 Write-Output "Building release $versionSuffix nuget packages..."
 dotnet pack --configuration Release --include-symbols --version-suffix $versionSuffix
 Write-Output "Moving $versionSuffix nuget packages to releases folder..."
@@ -12,3 +12,4 @@ If(!(test-path ..\releases))
 }
 Move-Item .\bin\Release\*.nupkg ..\releases -Force
 Write-Output "Done."
+popd
