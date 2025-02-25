ffb443ddbc457f3b28256c38ae24c36ecf415a8d
Dariusz Danielewski <dydek@hotmail.com>
2025-02-25 05:49:29 -0700

    Bug fix for TXTRecord and refactoring out CNameRecord

---
+++ Bdev.Net.Dns/Records/CNameRecord.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Bdev.Net.Dns.Records
+{
+    public class CNameRecord : RecordBase, IComparable, IEquatable<CNameRecord>
+    {
+        public string Value { get; set; }
+
+        internal CNameRecord(Pointer pointer)
+        {
+            Value = pointer.ReadDomain();
+        }
+
+        public bool Equals(CNameRecord other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
